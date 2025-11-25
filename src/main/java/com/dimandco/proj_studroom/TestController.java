3f23c7e10c968c4f2f611cd47c205f7c1dfa2d68
DimKark <it2023107@hua.gr>
2025-11-25 18:45:05 +0200

    1. Updated README 2. Disabled security dependencies 3. Modified application.properties into yaml format 4. Added a simple database implementation (You need to set up the database locally)

---
+++ src/main/java/com/dimandco/proj_studroom/TestController.java
@@ -0,0 +1,34 @@
+package com.dimandco.proj_studroom;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+/**
+ * Used for testing
+ */
+@RestController
+public class TestController {
+    private final PersonRepository personRepository;
+
+    public TestController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String test() {
+        Person person = new Person();
+        person.setId(null); // auto-generated
+        person.setEmail("example@hua.gr");
+        person.setFirstName("ExampleFirstName");
+        person.setLastName("ExampleLastName");
+        person.setPhoneNumber("+306900000000");
+        person.setCreatedAt(Instant.now());
+
+        person = this.personRepository.save(person);
+
+        return person.toString();
+    }
+}
