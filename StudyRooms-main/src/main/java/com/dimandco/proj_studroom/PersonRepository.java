b89f72ba43b1c3229952fc52e3e616117bf42f11
it2022005-hua <it2022005@hua.gr>
2025-11-27 12:27:00 +0200

    Delete StudyRooms-main directory

--- StudyRooms-main/src/main/java/com/dimandco/proj_studroom/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dimandco.proj_studroom;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
