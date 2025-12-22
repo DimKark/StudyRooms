2189f11d5f073a4672165b95c5b98e1e8c12399d
DimKark <it2023107@hua.gr>
2025-12-22 18:08:54 +0200

    Added SecurityConfig (The entire class is commented out for now)

---
+++ src/main/java/com/dimandco/proj_studroom/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.dimandco.proj_studroom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+/* SECURITY DEPENDENCIES
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+ */
+
+
+/**
+ * Security configuration.
+ */
+@Configuration
+//@EnableMethodSecurity
+public class SecurityConfig {
+//                                  TO BE UNCOMMENTED
+
+    // TODO API Security (stateless - JWT based)
+
+    /**
+     *  UI chain {@code "/**"} (stateful - cookie based)
+     */
+    /*
+    @Bean
+    @Order(2)
+    public SecurityFilterChain uiChain(final HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/**")
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login", "/register").permitAll() // Public
+                        .requestMatchers("/profile", "/logout").authenticated() // Private
+                        .anyRequest().permitAll() // The rest
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Custom Login Page
+                        .loginProcessingUrl("/login") // POST request target (handled by Spring Security)
+                        .defaultSuccessUrl("/profile", true)
+                        .failureUrl("/login?error")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // POST request target (handled by Spring Security)
+                        .logoutSuccessUrl("login?logout")
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
+                        .permitAll()
+                )
+                .httpBasic(basic -> {}) // Disable basic security
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+*/
+}
