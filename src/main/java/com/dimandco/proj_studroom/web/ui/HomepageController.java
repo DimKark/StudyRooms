2defe5eb9a5bcdea654af1880b716d09877e7989
DimKark <it2023107@hua.gr>
2026-01-04 19:17:30 +0200

    Implemented Thymeleaf Layout

---
+++ src/main/java/com/dimandco/proj_studroom/web/ui/HomepageController.java
@@ -0,0 +1,15 @@
+package com.dimandco.proj_studroom.web.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * UI controller for managing homepage
+ */
+@Controller
+public class HomepageController {
+    @GetMapping("/")
+    public String showHomepage() {
+        return "homepage";
+    }
+}
