0ec20b1266822b6a3d9a6a7f431924616b645215
DimKark <it2023107@hua.gr>
2026-01-19 14:16:03 +0200

    Removed temporary print statemets used for debugging

--- src/main/java/com/dimandco/proj_studroom/core/model/RoomReservation.java
+++ src/main/java/com/dimandco/proj_studroom/core/model/RoomReservation.java
@@ -110,22 +110,12 @@ public final class RoomReservation {
 
     /** Checks if given time overlaps with its own */
     public boolean overlaps(RoomReservation rr) {
-        System.out.println("Active: " + active);
         if(!active) return false;
-
-        StudyRoom r = rr.getRoom();
-        System.out.println("Is same room: " + this.room.equals(r));
-        if(!this.room.equals(r)) return false;
-
-        System.out.println("Is same date: " + date.equals(this.date));
-        LocalDate date = rr.getDate();
-        if(!date.equals(this.date)) return false;
+        if(!this.room.equals(rr.getRoom())) return false;
+        if(!rr.getDate().equals(this.date)) return false;
 
         LocalTime from =  rr.getFromTime();
         LocalTime to =  rr.getToTime();
-
-        System.out.println("Is within time: " + (from.isAfter(this.fromTime) && from.isBefore(this.toTime) ||
-                to.isAfter(this.fromTime) && to.isBefore(this.toTime)));
         return (from.isAfter(this.fromTime) && from.isBefore(this.toTime) ||
                 to.isAfter(this.fromTime) && to.isBefore(this.toTime));
     }
